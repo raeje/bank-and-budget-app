a69de9eda140b37177d25a38da792f42148605f3
raeje <rayanthony.eje@gmail.com>
2022-12-08 22:02:25 +0800

    Fix displaying expense items on Budget app (AddItems.js, Budget.js, ItemList.js, SetBudget.js)

--- src/routes/Budget.js
+++ src/routes/Budget.js
@@ -1,19 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import ItemList from "./ItemList";
 import AddItems from "./AddItems";
 import SetBudget from "./SetBudget";
 import "./Budget.css";
+import { getCurrentUser, getFilteredBudgetList } from "../utils/Util";
 
 const Budget = () => {
-    return (
-        <div className="budget-page glass">
-            <ItemList />
-            <div className="input-balance-container">
-                <AddItems />
-                <SetBudget />
-            </div>
-        </div>
-    );
+  const user = getCurrentUser();
+  const localBudgetList = getFilteredBudgetList("username", user.username);
+  const [budgetList, setBudgetList] = useState(localBudgetList);
+
+  return (
+    <div className="budget-page glass">
+      <ItemList list={budgetList} />
+      <div className="input-balance-container">
+        <AddItems setBudgetList={setBudgetList} />
+        <SetBudget />
+      </div>
+    </div>
+  );
 };
 
 export default Budget;
