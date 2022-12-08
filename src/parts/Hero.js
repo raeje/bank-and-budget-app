036bccf09006b544694af4b37fd1ed9e39ffce2a
raeje <rayanthony.eje@gmail.com>
2022-12-08 23:10:44 +0800

    [Home] UI changes; [LoginForm] added notifications

--- src/parts/Hero.js
+++ src/parts/Hero.js
@@ -7,6 +7,7 @@ function Hero(props) {
       <img src={props.img} alt={props.alt} />
       <span className="title">{props.title}</span>
       <span className="subtitle">{props.subtitle}</span>
+      <span className="hero-text-block">{props.textBlock}</span>
       {props.children}
     </section>
   );
