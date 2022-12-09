c6b21a980ee47f2c268d1bde7a1ae0d5f0105304
Marc Ponce <work.marcponce@gmail.com>
2022-12-09 21:18:06 +0800

    changed light theme colors

--- src/components/ChangeTheme.js
+++ src/components/ChangeTheme.js
@@ -1,15 +1,15 @@
 const ChangeTheme = (change) => {
   console.log("changing theme...", change);
   const root = document.querySelector(":root");
-  const darkWhite = "rgb(249, 246, 238)";
+  const darkWhite = "rgb(207, 207, 207)";
   const darkPrimary = "rgb(14, 17, 23)";
   const darkSecondary = "rgb(31, 35, 44)";
   const darkThird = "rgb(45, 51, 66)";
   const darkHighlight = "rgb(174, 171, 96)";
 
-  const lightWhite = "rgb(100, 100, 100)";
-  const lightSecondary = "rgb(255, 255, 255)";
-  const lightThird = "rgb(230, 230, 230)";
+  const lightWhite = "rgb(55, 55, 55)";
+  const lightSecondary = "rgb(192, 192, 192)";
+  const lightThird = "rgb(183, 183, 183)";
   const lightHighlight = "rgb(57, 91, 100)";
 
   root.style.setProperty("--primary", darkPrimary);
